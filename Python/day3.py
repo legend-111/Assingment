3999185c7c798993b0fafd58b86fdef183b7904d
Ujjwal Shrivastava <81064264+legend-111@users.noreply.github.com>
2022-02-20 13:49:51 +0530

    Add files via upload

---
+++ Python/day3.py
@@ -0,0 +1,75 @@
+# 1. Find the data type of a if a=9
+
+a=9
+print(type(a))
+
+# 2. Find the data type of a if a=9
+
+a=9
+print(type(a))
+
+# 3. Find the data type of a if a='9'
+
+a='9'
+print(type(a))
+
+# 4. Find the data type of a if a=(9)
+
+a=(9)
+print(type(a))
+
+#5. Find the data type of a if a=False
+
+a=False
+print(type(a))
+
+# 6. Find the data type of a if 
+
+a=[1,2,3]
+print(type(a))
+
+# 7. Find the data type of a if a=(1,2,3)
+
+a=(1,2,3)
+print(type(a))
+
+# 8. Find the data type of a if a={'key': 9}
+
+a={'key': 9}
+print(type(a))
+
+#9. Find the data type of a if a=1 + 9j
+
+a=1 + 9j
+print(type(a))
+
+#10. Set a=1 and b=2. What data type is a/b?
+a=1 
+b=2
+print(type(a/b))
+
+# 11. Create a dictionary numbers = {'one':1, 'two':2, 'three':3}. Pull out the number '2' by calling the key 'two'
+
+numbers = {'one':1, 'two':2, 'three':3}
+print(numbers['two'])
+
+#12. Create a tuple with the numbers 8, 9, and 10?
+
+t=(8,9,10)
+print(t)
+
+#13. Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+d={'one':1, 'two':2, 'three':3}
+print(d['one'])
+#errors = frist of all it is dictionary so we have to use 'one','two','three' for defining . for printing the value we have to use print statement 
+
+#14 Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+f = 'false' 
+not f
+# error = boolean expression is defiened as 'false' not false  
+
+# 1. Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+st = [1,3,5] 
+print(st[1])
+#error = there is only 2 indexing [0,1,2] thats why print is not working id you want to print 3 then you have to use [1] and variable is incorect doesnot start with numerical value
+
